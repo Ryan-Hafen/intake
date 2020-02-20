627f9807520de788c91e0a26c199db87ca8bd802
rhafen <rhafen@varis1.com>
2020-02-20 09:18:35 -0800

    moving app to package structure

--- srs_intake/models.py
+++ srs_intake/models.py
@@ -1,24 +1,15 @@
 from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
+from srs_intake import db
 
 class Facility(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=True)
-    address1 = db.Column(db.String(120), unique=True, nullable=True)
-    address2 =db.Column(db.String(120), unique=True, nullable=True)
-    city = db.Column(db.String(20), unique=True, nullable=True)
-    state = db.Column(db.String(2), unique=True, nullable=True)
-    zip_code = db.Column(db.String(10), unique=True, nullable=True)
-    source = db.Column(db.String(10), unique=True, nullable=True)
+    name = db.Column(db.String(20), nullable=True)
+    address1 = db.Column(db.String(120), nullable=True)
+    address2 =db.Column(db.String(120), nullable=True)
+    city = db.Column(db.String(20), nullable=True)
+    state = db.Column(db.String(2), nullable=True)
+    zip_code = db.Column(db.String(10), nullable=True)
+    source = db.Column(db.String(10), nullable=True)
     users = db.relationship('User', backref='user_loc', lazy=True)
     referals = db.relationship('Referral', backref='referral_loc', lazy=True)
 
@@ -28,12 +19,12 @@ class Facility(db.Model):
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(20), unique=True, nullable=True)
-    lastname = db.Column(db.String(20), unique=True, nullable=True)
+    firstname = db.Column(db.String(20), nullable=True)
+    lastname = db.Column(db.String(20), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=True)
     phone = db.Column(db.String(20), unique=True, nullable=True)
-    fax = db.Column(db.String(20), unique=True, nullable=True)
-    role = db.Column(db.String(20), unique=True, nullable=True)
+    fax = db.Column(db.String(20), nullable=True)
+    role = db.Column(db.String(20), nullable=True)
     username = db.Column(db.String(20), unique=True, nullable=True)
     password = db.Column(db.String(60), nullable=True)
     facility_id = db.Column(db.Integer, db.ForeignKey('facility.id'), nullable=True)
@@ -105,55 +96,3 @@ class Referral(db.Model):
 
     def __repr__(self):
         return f"Referral('{self.firstname}', '{self.lastname}', '{self.discharge_date}')" 
-
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html', posts=posts)
-
-
-@app.route("/about")
-def about():
-    return render_template('about.html', title='About')
-
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
---
+++ srs_intake/srs_intake.py
@@ -0,0 +1,4 @@
+from srs_intake import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
