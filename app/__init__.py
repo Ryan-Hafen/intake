c061b42c4fba84aad88c1b92065e1207e5977ae7
rhafen <rhafen@varis1.com>
2020-03-05 15:26:12 -0800

    added session timeout

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from cryptography.fernet import Fernet
 from flask_login import LoginManager
 from flask_mail import Mail
+from datetime import timedelta
 
 app = Flask(__name__)
 
@@ -27,4 +28,12 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
+login_manager.refresh_view = 'users.login'
+login_manager.needs_refresh_message = (u"Session timedout, please re-login")
+login_manager.needs_refresh_message_category = "info"
 mail = Mail(app)
+
+@app.before_request
+def before_request():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=120)
