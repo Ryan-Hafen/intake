52aa97f9c36fbeb77b34ec5416e3effcb83ca45d
Ryan Hafen <ryan.hafen@icloud.com>
2020-03-06 22:23:29 -0800

    final deployment updates

--- app/main/routes.py
+++ app/main/routes.py
@@ -11,11 +11,5 @@ main = Blueprint('main', __name__)
 @main.route("/home")
 def home():
     if current_user.is_authenticated:
-        user = User.query.get(current_user.id)
-        if user.role == 'source':
-            referrals = Referral.query.filter_by(source_id=user.source_id)
-            return render_template('referrals/referral_list.html', title="Referrals")
-        if user.role == 'admin':
-            referrals = Referral.query.all()
-            return render_template('referrals/referral_list.html', title="Referrals")
+        return redirect(url_for('referrals.list_referrals'))
     return redirect(url_for('users.login'))
